ac2a10e9675b369796e4aaeece9e457415ddfc91
FelipeMurillo1 <lmurillomatallana@gmail.com>
2024-05-19 18:09:14 -0500

    app.v2

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 document.addEventListener('DOMContentLoaded', function () {
     const orderForm = document.getElementById('order-form');
     const orderSummary = document.getElementById('order-summary');
@@ -5,11 +6,15 @@ document.addEventListener('DOMContentLoaded', function () {
 
     orderForm.addEventListener('submit', function (e) {
         e.preventDefault();
-        
+
+        const customerType = document.getElementById('customer-type').value;
+        const orderType = document.getElementById('order-type').value;
+        const tableNumber = document.getElementById('table-number').value;
         const menuItems = document.querySelectorAll('.menu-item-checkbox');
+
         let selectedItems = [];
         let totalPrice = 0;
-        
+
         menuItems.forEach(item => {
             if (item.checked) {
                 const itemName = item.getAttribute('data-name');
@@ -18,14 +23,19 @@ document.addEventListener('DOMContentLoaded', function () {
                 totalPrice += itemPrice;
             }
         });
-        
+
         // Crear el resumen del pedido
         let summary = '';
         selectedItems.forEach(item => {
             summary += `${item.name} - $${item.price.toFixed(2)}\n`;
         });
         summary += `\nTotal: $${totalPrice.toFixed(2)}`;
-        
+
+        // Añadir información de la mesa si está disponible
+        if (customerType === 'Mesero' && tableNumber) {
+            summary += `\nNúmero de Mesa: ${tableNumber}`;
+        }
+
         // Mostrar el resumen y el total
         totalDiv.classList.remove('hidden');
         orderSummary.textContent = summary;
